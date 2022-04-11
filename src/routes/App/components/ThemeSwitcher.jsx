3af2850b459f317d4b15c8b73cc9cb6537b8e668
Amir <amirhatam136@gmail.com>
2022-04-11 11:08:19 +0200

    add sun icon and create state for colors & icons

--- src/routes/App/components/ThemeSwitcher.jsx
+++ src/routes/App/components/ThemeSwitcher.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function ThemeSwitcher() {
+    const [fontawesome, setFontawesome] = useState(faMoon)
+    const [iconcolor, setIconColor] = useState("")
 
     //Returns the first child element that matches a CSS selector(s)
     const html = document.querySelector('html');
@@ -12,15 +14,19 @@ function ThemeSwitcher() {
     function switchTheme() {
         if (html.className) {
             html.className = "";
+            setFontawesome(faMoon)
+            setIconColor("#000")
         } else {
             html.className = "dark-mode";
+            setFontawesome(faSun)
+            setIconColor("#FFA500")
         }
     }
 
     return (
         <>
             <button onClick={switchTheme} className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} />
+                <FontAwesomeIcon color={iconcolor} icon={fontawesome} />
             </button>
         </>
     )
